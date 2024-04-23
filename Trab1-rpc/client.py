51221f4e82573263ff86e9dcaf3370277140b3eb
JonatasP2A <jonatasalves@id.uff.br>
2024-04-23 19:21:21 -0300

    server and client as doc

--- Trab1-rpc/client.py
+++ Trab1-rpc/client.py
@@ -1,29 +1,40 @@
 import rpyc
 import sys
 import os
+import time
 from random import randint
 
+
 def criarVetor():
-	# cria lista vazia
-	vetor = []
-	# aguarda o input do cliente do tamanho do vetor
-	tamanho_vetor = int(input("Insira o tamanho do vetor: "))
-	
-	# adiciona na lista um valor variando de 0 até tamanho_vetor -1
-	for i in range(tamanho_vetor):
-		vetor.append(randint(0, tamanho_vetor -1))
-	# retorna vetor preenchido
-	return vetor
+   # cria lista vazia
+   vetor = []
+   # aguarda o input do cliente do tamanho do vetor
+   tamanho_vetor = int(input("Insira o tamanho do vetor: "))
+  
+   # adiciona na lista um valor variando de 0 até tamanho_vetor -1
+   for i in range(tamanho_vetor):
+       vetor.append(randint(0, tamanho_vetor -1))
+   # retorna vetor preenchido
+   return vetor
 
-if len(sys.argv) < 2:
-  exit("Usage {} SERVER".format(sys.argv[0]))
 
-server = sys.argv[1]
-conn = rpyc.connect(server,18861)
-print(conn.root)
-print(conn.root.get_answer())
-print(conn.root.the_real_answer_though)
-# chama função para criar o vetor
-vetor_cliente = criarVetor()
-# executa o método exposto do servidor para fazer a soma
-soma_vetor = conn.root.exposed_somaVetor(vetor_cliente)
+if __name__ == "__main__":
+   if len(sys.argv) != 2:
+       print("Usage: {} SERVER".format(sys.argv[0]))
+       sys.exit(1)
+   server = sys.argv[1]
+   conn = rpyc.connect('192.168.0.200', 18861)
+   print(conn.root)
+   print(conn.root.get_answer())
+   print(conn.root.the_real_answer_though)
+  
+   # chama função para criar o vetor
+   start = time.time()
+   vetor_cliente = criarVetor()
+   # executa o método exposto do servidor para fazer a soma
+  
+   soma_vetor = conn.root.exposed_somaVetor(vetor_cliente)
+   end = time.time()
+   print(f"O tempo para execução da função no cliente foi de: {end-start} \n")
+   # Printa no terminal o valor da soma calculado pelo servidor
+   print(f"A soma, calculada pelo Servidor, dos elementos do vetor é: {soma_vetor} \n")
--- Trab1-rpc/server.py
+++ Trab1-rpc/server.py
@@ -1,38 +1,37 @@
 import rpyc
-import time
+from rpyc.utils.server import ThreadedServer
 
 class MyService(rpyc.Service):
-  def on_connect(self, conn):
-    self._conn = conn
-    # código que é executado quando uma conexão é iniciada, caso
-    # seja necessário
-    pass
+	# método ao se conectar
+	def on_connect(self, conn):
+		print("SERVIDOR - Cliente conectado ao Servidor! \n")
+		pass
+	
+	# método ao se desconectar
+	def on_disconnect(self, conn):
+		print("SERVIDOR - Cliente desconectado ao Servidor! Adeus! \n")
+		pass
+	
+	# este é um método exposto
+	def exposed_get_answer(self):
+		return 42
+	
+	# este é um atributo exposto
+	exposed_the_real_answer_though = 43
 
-  def on_disconnect(self, conn):
-    # código que é executado quando uma conexão é finalizada,
-    # caso seja necessário
-    pass
-
-  def exposed_get_answer(self): # este é um método exposto
-    return 42
-
-  exposed_the_real_answer_though = 43 # este é um atributo exposto
-
-  def get_question(self): # este método não é exposto
-    return "Qual é a cor do cavalo branco de Napoleão?"
-    #Para iniciar o servidor
-  
-  # método exposto para somar os elementos do vetor
-  def exposed_somaVetor(self, vetor):
-    start = time.time()
-    soma = sum(vetor)
-    end = time.time()
-    print(end-start)
-    return soma
+	# este não é um método exposto
+	def get_question(self):
+		return "Qual é a cor do cavalo branco do Napoleão?"
+	
 
 
+
+# método exposto para somar os elementos do vetor
+def exposed_somaVetor(self, vetor):
+	soma = sum(vetor)
+	return soma
+
+# para inciar o servidor
 if __name__ == "__main__":
-  from rpyc.utils.server import ThreadedServer
-
-  t = ThreadedServer(MyService, port=18861)
-  t.start()
+	t = ThreadedServer(MyService, port =18861)
+	t.start()
